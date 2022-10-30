cbf1ed25aeba0a6c199fc0c7844027e743ee9df5
skreddykoppula <98203945+skreddykoppula@users.noreply.github.com>
2022-10-30 20:33:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.get("/", function (request, response) {
   response.send("Hello World");
 });
 
+// eslint-disable-next-line no-unused-vars
 app.get("/todos", async function (_request, response) {
   console.log("Processing list of all Todos ...");
   // FILL IN YOUR CODE HERE
@@ -15,6 +16,14 @@ app.get("/todos", async function (_request, response) {
   // First, we have to query our PostgerSQL database using Sequelize to get list of all Todos.
   // Then, we have to respond with all Todos, like:
   // response.send(todos)
+
+  try {
+    const todos = await Todo.findAll({ order: [["id", "ASC"]] });
+    return response.json(todos);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
 });
 
 app.get("/todos/:id", async function (request, response) {
@@ -50,11 +59,13 @@ app.put("/todos/:id/markAsCompleted", async function (request, response) {
 
 app.delete("/todos/:id", async function (request, response) {
   console.log("We have to delete a Todo with ID: ", request.params.id);
+
   // FILL IN YOUR CODE HERE
 
   // First, we have to query our database to delete a Todo by ID.
+  const affectedRow = await Todo.destroy({ where: { id: request.params.id } });
   // Then, we have to respond back with true/false based on whether the Todo was deleted or not.
-  // response.send(true)
+  response.send(affectedRow ? true : false);
 });
 
 module.exports = app;
