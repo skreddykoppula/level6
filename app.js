28fdd3c34523be413958c1dcf96025df96b5b232
skreddykoppula <98203945+skreddykoppula@users.noreply.github.com>
2022-12-28 16:28:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,52 @@
 const express = require("express");
 const app = express();
+const Sequelize = require("sequelize");
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
+const Op = Sequelize.Op;
+const todo = require("./models/todo");
+
 app.use(bodyParser.json());
 
-app.get("/", function (request, response) {
-  response.send("Hello World");
-});
-
-// eslint-disable-next-line no-unused-vars
-app.get("/todos", async function (_request, response) {
+app.get("/todos", async function (request, response) {
   console.log("Processing list of all Todos ...");
-  // FILL IN YOUR CODE HERE
-
-  // First, we have to query our PostgerSQL database using Sequelize to get list of all Todos.
-  // Then, we have to respond with all Todos, like:
-  // response.send(todos)
-
   try {
-    const todos = await Todo.findAll({ order: [["id", "ASC"]] });
-    return response.json(todos);
+    // create a todo
+    const date = new Date();
+    await Todo.addTodo({ title: "Test", dueDate: date, completed: false });
+
+    const todos = await Todo.findAll();
+    console.log(todos);
+    const d = new Date().toLocaleDateString("en-CA");
+    const overdue = await Todo.findAll({
+      where: { dueDate: { [Op.lt]: d }, completed: false },
+      order: [["id", "ASC"]],
+    });
+    const overdueComplete = await Todo.findAll({
+      where: { dueDate: { [Op.lt]: d }, completed: true },
+    });
+    const later = await Todo.findAll({
+      where: { dueDate: { [Op.gt]: d } },
+    });
+    const laterComplete = await Todo.findAll({
+      where: { dueDate: { [Op.gt]: d }, completed: true },
+    });
+    const today = await Todo.findAll({
+      where: { dueDate: { [Op.eq]: d } },
+    });
+    const todayComplete = await Todo.findAll({
+      where: { dueDate: { [Op.eq]: d }, completed: true },
+    });
+
+    app.locals.tasks = todos;
+    app.locals.overdue = overdue;
+    app.locals.overdueComplete = overdueComplete;
+    app.locals.later = later;
+    app.locals.laterComplete = laterComplete;
+    app.locals.today = today;
+    app.locals.todayComplete = todayComplete;
+
+    response.render("index");
   } catch (error) {
     console.log(error);
     return response.status(422).json(error);
@@ -27,8 +54,9 @@ app.get("/todos", async function (_request, response) {
 });
 
 app.get("/todos/:id", async function (request, response) {
+  console.log("Looking for Todo with ID: ", request.params.id);
   try {
-    const todo = await Todo.findByPk(request.params.id);
+    const todo = await todo.findByPk(request.params.id);
     return response.json(todo);
   } catch (error) {
     console.log(error);
@@ -37,6 +65,7 @@ app.get("/todos/:id", async function (request, response) {
 });
 
 app.post("/todos", async function (request, response) {
+  console.log("Creating new Todo: ", request.body);
   try {
     const todo = await Todo.addTodo(request.body);
     return response.json(todo);
@@ -47,6 +76,7 @@ app.post("/todos", async function (request, response) {
 });
 
 app.put("/todos/:id/markAsCompleted", async function (request, response) {
+  console.log("We have to update a Todo with ID: ", request.params.id);
   const todo = await Todo.findByPk(request.params.id);
   try {
     const updatedTodo = await todo.markAsCompleted();
@@ -59,13 +89,19 @@ app.put("/todos/:id/markAsCompleted", async function (request, response) {
 
 app.delete("/todos/:id", async function (request, response) {
   console.log("We have to delete a Todo with ID: ", request.params.id);
+  try {
+    const todo = await todo.findByPk(request.params.id);
+    todo.delete();
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
 
-  // FILL IN YOUR CODE HERE
-
-  // First, we have to query our database to delete a Todo by ID.
-  const affectedRow = await Todo.destroy({ where: { id: request.params.id } });
-  // Then, we have to respond back with true/false based on whether the Todo was deleted or not.
-  response.send(affectedRow ? true : false);
+app.get("/", (request, response) => {
+  app.locals.tasks = [{ title: "taks 1" }, { title: "task 2" }];
+  response.render("index");
 });
 
 module.exports = app;
